d3f799c10634b838316830eab3c229c7d02fa22c
aguair-dev-cs <franciscowededasilvaaguiar@gmail.com>
2025-04-25 09:00:18 -0400

    realizado exercício 12

--- src/main/java/fundamentos/Exer12.java
+++ src/main/java/fundamentos/Exer12.java
@@ -1,7 +1,7 @@
 package fundamentos;
 
 /*
-Tendo como dados de entrada a altura da pessoa, construa um
+Tendo como dados de entrada a altura de uma pessoa, construa um
 algoritmo que calcule seu peso ideal, usando a seguinte fórmula:
 (72.7*altura) - 58
 */
